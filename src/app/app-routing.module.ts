43d38a659602a97c5e7d3b079a417558e6c47c2e
flvasques <fflqvn@gmail.com>
2022-10-24 20:19:11 -0300

    FEAT: basics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
+import { NotFoundComponent } from './not-found/not-found.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+
+  //Wild Card Route for 404 request
+  {
+    path: '**',
+    pathMatch: 'full',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
